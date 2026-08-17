6b72e9c4719dd5b263e2eb9e572145cda3b0925e
adrianhelllo <hetessyadrian@gmail.com>
2026-08-17 18:59:13 +0200

    fix script for button

--- frontend/src/components/Clock.tsx
+++ frontend/src/components/Clock.tsx
@@ -1,30 +1,35 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import ButtonCont from "./ButtonCont";
 
 export default function Clock() {
     const dur = 3000; // 50min
 
     let [remaining, setRemaining] = useState(dur);
+    let [counting, setCounting] = useState(false);
 
     useEffect(() => {
-        const countdown = setInterval(() => {
-            setRemaining(prev => { // Errors with type assignment number | void: means sometimes void = nothing is returned
-                if (prev <= 1) {
-                    clearInterval(countdown);
-                    return 0
-                }
+        // Run on load to set timer
+        if (counting) {
+            const countdown = setInterval(() => {
+                setRemaining((prev) => {
+                    // Errors with type assignment number | void: means sometimes void = nothing is returned
+                    if (prev <= 1) {
+                        clearInterval(countdown);
+                        return 0;
+                    }
 
-                return prev - 1
-            })
-        }, 1000);
+                    return prev - 1;
+                });
+            }, 1000);
 
-        return () => clearInterval(countdown) // The interval persists page rerender, therefore cleaning the interval up if the effect is cut short is a good idea
-    }, []);
+            return () => clearInterval(countdown); // The interval persists page rerender, therefore cleaning the interval up if the effect is cut short is a good idea
+        }
+    }, [counting]);
 
     const minutes = Math.floor(remaining / 60);
     const seconds = remaining % 60;
 
-    const time = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    const time = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 
     return (
         <div className="clock">
@@ -33,7 +38,15 @@ export default function Clock() {
             </div>
             <div className="clock__watch">
                 <div className="clock__timer">{time}</div>
-                <button className="clock__actionbtn">start / stop</button>
+                <button
+                    className="clock__toggle"
+                    onClick={() => {
+                        console.log("clicked");    
+                        setCounting(!counting);
+                    }}
+                >
+                    {counting ? 'stop' : 'start'}
+                </button>
             </div>
         </div>
     );
