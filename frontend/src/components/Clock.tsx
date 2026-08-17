0c7288d1dc5e71e4cf237b431907e93c821844a8
adrianhelllo <hetessyadrian@gmail.com>
2026-08-17 18:13:37 +0200

    create primitive countdown component

--- frontend/src/components/Clock.tsx
+++ frontend/src/components/Clock.tsx
@@ -3,13 +3,28 @@ import ButtonCont from "./ButtonCont";
 
 export default function Clock() {
     const dur = 3000; // 50min
-    const [h, m] = [Math.floor(dur / 60), dur % 60];
 
-    const [remaining, setRemaining] = useState(dur);
+    let [remaining, setRemaining] = useState(dur);
 
     useEffect(() => {
-        
-    }, [dur])
+        const countdown = setInterval(() => {
+            setRemaining(prev => { // Errors with type assignment number | void: means sometimes void = nothing is returned
+                if (prev <= 1) {
+                    clearInterval(countdown);
+                    return 0
+                }
+
+                return prev - 1
+            })
+        }, 1000);
+
+        return () => clearInterval(countdown) // The interval persists page rerender, therefore cleaning the interval up if the effect is cut short is a good idea
+    }, []);
+
+    const minutes = Math.floor(remaining / 60);
+    const seconds = remaining % 60;
+
+    const time = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 
     return (
         <div className="clock">
@@ -17,7 +32,7 @@ export default function Clock() {
                 <ButtonCont></ButtonCont>
             </div>
             <div className="clock__watch">
-                <div className="clock__timer">{h}:{m}</div>
+                <div className="clock__timer">{time}</div>
                 <button className="clock__actionbtn">start / stop</button>
             </div>
         </div>
